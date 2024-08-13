3c87c3d559d3aba6f019416446e8486ca577b5a4
Neil Crosswaite <43375279+neilCrosswaite@users.noreply.github.com>
2024-08-13 14:27:17 +0100

    remove bad iris example

--- src/mutated/iris_utility.py
+++ src/mutated/iris_utility.py
@@ -6,16 +6,6 @@ def round_down_to_1(cube):
     If less than 1, stay the same
     Aware of the mask.
     """
-    cube.data = np.ma.where(cube.data > 1, 1, cube.data)
+    cube.data = np.ma.where(cube.data > 1, -1, cube.data)
+    cube.attributes['history'] = 'round_down_to_1'
     return cube
-
-def round_down_to_1_bad(cube):
-    """
-    Round down the data in the cube to a maximum of 1.
-    If less than 1, stay the same
-    Aware of the mask.
-    """
-    above_one = np.where((cube.core_data() > 1), True, False)
-    cube.core_data()[above_one] = 1
-    return cube
-
