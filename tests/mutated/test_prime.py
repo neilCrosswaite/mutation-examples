da8ce6fe5e55b56010386d44b1a5f33919611998
Neil Crosswaite <43375279+neilCrosswaite@users.noreply.github.com>
2024-08-14 13:03:00 +0100

    change tests for prime

--- tests/mutated/test_prime.py
+++ tests/mutated/test_prime.py
@@ -2,11 +2,13 @@ import pytest
 from mutated.prime import is_prime
 
 def test_is_prime():
+    # chat-gpt
     assert is_prime(2)
     assert is_prime(3)
     assert is_prime(5)
     assert is_prime(7)
     assert is_prime(31)
+    # assert is_prime(9999999900000001)
 
 
     # assert not is_prime(1)
