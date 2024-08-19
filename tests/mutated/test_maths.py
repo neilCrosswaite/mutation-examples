7a7aa7654b93ba597518009f60165a8a7b610395
Neil Crosswaite <43375279+neilCrosswaite@users.noreply.github.com>
2024-08-19 14:48:02 +0100

    2 plus 2

--- tests/mutated/test_maths.py
+++ tests/mutated/test_maths.py
@@ -4,6 +4,7 @@ from mutated.maths import add, is_within_range, multiply, divide
 def test_add():
     # me
     assert add(1, 2) == 3
+    assert add(2, 2) == 4
     assert add(-1, 1) == 0
     assert add(-1, -1) == -2
 
