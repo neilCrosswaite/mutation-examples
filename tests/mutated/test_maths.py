71c87cc415b859e148211c3ab2b918908fa71589
Neil Crosswaite <43375279+neilCrosswaite@users.noreply.github.com>
2024-08-19 13:01:28 +0100

    rearrange test maths

--- tests/mutated/test_maths.py
+++ tests/mutated/test_maths.py
@@ -7,12 +7,6 @@ def test_add():
     assert add(-1, 1) == 0
     assert add(-1, -1) == -2
 
-def test_within_range():
-    # me
-    assert is_within_range(5, 1, 10)
-    assert not is_within_range(0, 1, 10)
-    assert not is_within_range(11, 1, 10)
-
 def test_multiply():
     # co-pilot
     assert multiply(1, 2) == 2
@@ -29,3 +23,9 @@ def test_divide():
     # Test divide by zero and error message regex
     with pytest.raises(ValueError):
         divide(1, 0)
+
+def test_within_range():
+    # me
+    assert is_within_range(5, 1, 10)
+    assert not is_within_range(0, 1, 10)
+    assert not is_within_range(11, 1, 10)
